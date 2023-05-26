f6f3b0df0c71c5e08e2266c63184ad63fa597a1a
kevinalg8 <kalondono35@soy.sena.edu.co>
2023-05-26 00:05:57 -0500

    partials

--- app/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-
-const product = Router();
-
-product.get('/productos', (req, res)=>{res.render("Productos")})
-
-
-export default product;
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,7 +1,7 @@
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function() {
-    if (window.pageYOffset > 100) {
+    if (window.pageYOffset > 350) {
         nav.classList.add('bg-dark', 'shadow');
     } else {
         nav.classList.remove('bg-dark', 'shadow');
