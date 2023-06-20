df6ba08426ddf7377f2fa2f2f328126772a758b7
jhonatan032004 <jpalacio1137@gmail.com>
2023-06-20 14:57:56 -0500

    inhabilitacion

--- app/controllers/controller.usuarios.js
+++ app/controllers/controller.usuarios.js
@@ -23,9 +23,36 @@ export const getUsuarios = async (req, res) => {
   }
 };
 
-export const inhabilitar = (req, res) => {
-    let estado = req.query.estado
-
-    let ruta = "http:localhost//3000/api/users/" + req.query.id
-    res.send(ruta+ " ESTADO : "+estado)
+export const inhabilitar = async(req, res) => {
+  let estado= req.query.estado
+  if (estado == 1) {
+    estado = 0
+  } else {
+    estado = 1
   }
+  try {  
+    let data ={
+        ESTADO:estado
+      }
+      console.log(data);
+      let ruta = `http://localhost:3000/api/users/${req.query.id}`;
+      let option = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      };
+        console.log(data);
+        let estad = {};
+        const resultado = await fetch(ruta, option)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+            })
+            .catch(err => console.error("Error en peticion: " + err));
+            res.redirect("/admin")
+          } catch (error) {
+      console.log(error);
+  }
+}
