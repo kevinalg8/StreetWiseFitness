0fbe2357ddc3c96848f3e21ab9bacd71eed7be58
kevinalg8 <kalondono35@soy.sena.edu.co>
2023-06-28 21:21:35 -0500

    recetas

--- app/controllers/controller.recetas.js
+++ app/controllers/controller.recetas.js
@@ -22,4 +22,36 @@ export const getReceta = async (req, res) => {
     } catch (error) {
       console.log(error);
     }
-  };
+};
+
+export const crearReceta = async (req, res) => {
+  if(req.body.NOMBRE && req.body.DESCRIPCION){
+
+      let data = {
+        NOMBRE: req.body.NOMBRE,
+        DESCRIPCION: req.body.DESCRIPCION
+      }
+      let metodo = "POST";
+      let url = process.env.API_URL + '/rec';
+      let option = {
+        method: metodo,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    try {
+      const respuesta = fetch(url, option)
+          .then(response => response.json())
+          .then(data =>
+              //data:data
+              console.log(`Receta Creada`))
+          .catch(err => console.log(`Error: ${err}`))
+  } catch (error) {
+      console.log(`error en ${error}`);
+  }
+  //res.redirect("/")
+  }
+
+}
--- app/controllers/controller.usuarios.js
+++ app/controllers/controller.usuarios.js
@@ -2,6 +2,7 @@ import fetch from "node-fetch";
 import jwt from "jsonwebtoken";
 import axios from "axios";
 import PDFDocument from "pdfkit-table";
+import path from 'path';
 
 export const getUsuarios = async (req, res) => {
   try {
@@ -57,9 +58,40 @@ export const loginUsuario = async (req, res) => {
           if (datosDb.COD_ROL === 1 || datosDb.COD_ROL ===2) {
             res.render("inicio", {
               "user": datosDb,
-              "rol": datosDb.COD_ROL
+              "rol": datosDb.COD_ROL,
+              "cod_User":datosDb.COD_USUARIO
             });
-            
+            console.log(datosDb.COD_USUARIO);
+            try {
+              console.log(datosDb.COD_USUARIO);
+              let data = {
+                NOMBRE: req.body.NOMBRE,
+                DESCRIPCION: req.body.DESCRIPCION,
+                COD_USUARIO: req.body.COD_USUARIO
+              }
+              let metodo = "POST";
+              let url = process.env.API_URL + '/rec';
+              let option = {
+                method: metodo,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }
+        
+            try {
+              const respuesta = fetch(url, option)
+                  .then(response => response.json())
+                  .then(data =>
+                      //data:data
+                      console.log(`Receta Creada`))
+                  .catch(err => console.log(`Error: ${err}`))
+          } catch (error) {
+              console.log(`error en ${error}`);
+          }
+            } catch (error) {
+              
+            }
           }
           if (datosDb.COD_ROL == 3) {
             try {
@@ -158,12 +190,12 @@ export const generarPdf = async (req, res) => {
     doc.pipe(res);
 
     // Agregar el logo del proyecto
-    // const logoHeight = 50;
-    // const logoWidth = 50;
-    // const __dirname = path.resolve()
-    // const imagePath = path.resolve(path.join(__dirname, 'public', 'img', 'logoSena.png')) ;
-    // const logoX = (pageWidth - logoWidth) / 2;
-    // const logoY = 30;
+     const logoHeight = 50;
+     const logoWidth = 50;
+     const __dirname = path.resolve()
+     const imagePath = path.resolve(path.join(__dirname, 'app', 'public', 'img', 'Logo.png')) ;
+     const logoX = (pageWidth - logoWidth) / 2;
+     const logoY = 30;
 
     const pageWidth = doc.page.width;
     const pageHeight = doc.page.height;
@@ -200,7 +232,7 @@ export const generarPdf = async (req, res) => {
     await doc.table(table, { width: 500, prepareHeader: () => doc.font('Helvetica-Bold').fontSize(10), prepareRow: () => doc.font('Helvetica').fontSize(10) });
 
     // Agregar el pie de página
-    const generador = 'StreetWise';
+    const generador = 'StreetWise Fitness';
     const fechaImpresion = new Date().toLocaleString();
     doc.fontSize(10).text(`Generado por: ${generador}`);
     doc.fontSize(10).text(`Fecha y hora de impresión: ${fechaImpresion}`, { align: 'right' });
