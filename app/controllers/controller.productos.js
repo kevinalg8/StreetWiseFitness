5d8ede8d86261c62efec88f8387ce91538d1380d
camilagv1 <danielajaraga@gmail.com>
2023-07-13 16:58:16 -0500

    Comentarios de controllers completo

--- app/controllers/controller.productos.js
+++ app/controllers/controller.productos.js
@@ -1,5 +1,7 @@
 import fetch from "node-fetch";
 
+
+//Productos en general
 export const getProductos = async (req, res) => {
   try {
     let url1 = 'https://api.mercadolibre.com/sites/MCO/search?nickname=TUMEJORCOMPRA0&limit=5';
@@ -10,22 +12,26 @@ export const getProductos = async (req, res) => {
     let option = {
       method: "GET"
     };
-
+    // Obtener datos de la URL 1
     const response1 = await fetch(url1, option);
     const data1 = await response1.json();
     //console.log(data1);
 
+    // Obtener datos de la URL 2
     const response2 = await fetch(url2, option);
     const data2 = await response2.json();
     //console.log(data2);
 
+    // Obtener datos de la URL 3
     const response3 = await fetch(url3, option);
     const data3 = await response3.json();
     //console.log(data3);
 
+    // Obtener datos de la URL 4
     const response4 = await fetch(url4, option);
     const data4 = await response4.json();
 
+    // Extraer los productos y vendedores de cada respuesta
     let productos1 = data1.results;
     let vendedor1 = data1.seller;
     let productos2 = data2.results;
@@ -35,6 +41,7 @@ export const getProductos = async (req, res) => {
     let productos4 = data4.results;
     let vendedor4 = data4.seller; 
 
+    // Renderizar la vista "productos" y pasar los datos obtenidos
     res.render("productos", {
       "produc1": productos1,
       "seller1": vendedor1,
@@ -60,14 +67,16 @@ export const getProdinf = async (req, res) => {
       method: "GET"
     };
 
+    // Obtener datos de la URL
     const response = await fetch(url1, option);
     const data = await response.json();
     console.log(data);
 
+    // Extraer los productos y vendedores de la respuesta
     let productos = data.results;
     let vendedor = data.seller;
 
-
+    // Renderizar la vista "trenInferior" y pasar los datos obtenidos
     res.render("trenInferior", {
       "produc1": productos,
       "seller1": vendedor,
@@ -87,14 +96,16 @@ export const getProdsup = async (req, res) => {
       method: "GET"
     };
 
+    // Obtener datos de la URL
     const response = await fetch(url2, option);
     const data = await response.json();
     console.log(data);
 
+    // Extraer los productos y vendedores de la respuesta
     let productos = data.results;
     let vendedor = data.seller;
 
-
+    // Renderizar la vista "trenSuperior" y pasar los datos obtenidos
     res.render("trenSuperior", {
       "produc2": productos,
       "seller2": vendedor,
@@ -114,14 +125,16 @@ export const getProdgen = async (req, res) => {
       method: "GET"
     };
 
+    // Obtener datos de la URL
     const response = await fetch(url3, option);
     const data = await response.json();
     console.log(data);
 
+    // Extraer los productos y vendedores de la respuesta
     let productos = data.results;
     let vendedor = data.seller;
 
-
+    // Renderizar la vista "planGeneral" y pasar los datos obtenidos
     res.render("planGeneral", {
       "produc3": productos,
       "seller3": vendedor,
