e1ecaa913c366521c27dc631266c62d9a39ff0f7
camilagv1 <danielajaraga@gmail.com>
2023-07-13 17:17:26 -0500

    Comentarios completos

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 
+// Iniciar el servidor Express y escuchar en el puerto especificado
 app.listen(app.get("port"), ()=> {
     console.log(`Se ha conectado al puerto ${app.get("port")}
     http://localhost:${app.get("port")}`);
